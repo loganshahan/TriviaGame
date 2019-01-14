1f6970ea8fa09b0868850abc824990b296d33393
Logan Shahan <loganshahan88@gmail.com>
2019-01-14 17:17:06 -0500

    added q's

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TriviaGame
+
+Select the correct response before the timer runs out.  This quiz is about Dallas Cowboys history.
---
+++ javascript/app.js
@@ -0,0 +1,56 @@
+var questions = [
+	{
+		question: "Who holds the record for most rushing yards?",
+		answers: {
+			a: 'Emmit Smith',
+			b: 'Zeke Williams',
+			c: 'Hershell Walker'
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "Who was the last quarterback to win a Super Bowl?",
+		answers: {
+			a: 'Tony Romo',
+			b: 'Roger Staubach',
+			c: 'Troy Aikman'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "What is the nickname for the new stadium?",
+		answers: {
+			a: 'AT&T Field',
+			b: 'Jerry World',
+			c: 'The Star'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What are the uniform colors?",
+		answers: {
+			a: 'black and yellow',
+			b: 'red and blue',
+			c: 'blue and silver'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "How many Super Bowls have the Cowboys won?",
+		answers: {
+			a: '5',
+			b: '4',
+			c: '0'
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "Who is the current starting quarterback?",
+		answers: {
+			a: 'Tony Romo',
+			b: 'Troy Aikman',
+			c: 'Dak Prescott'
+		},
+		correctAnswer: 'c'
+    },
+];
